9fcd8efae8a2e18cd7b547c4875765177b91c744
Konstantin Kosmatov <key@kosmatov.su>
2012-09-04 15:57:58 +0400

    new email regexp validator

--- lib/validates/email_validator.rb
+++ lib/validates/email_validator.rb
@@ -1,6 +1,8 @@
 class EmailValidator < ActiveModel::EachValidator
+  include EmailRegexp
+
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    unless regexp_compare(value)
       record.errors.add(attribute, :email, options.merge(:value => value))
     end
   end
