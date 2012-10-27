0ca4a101515e6843e4d230f3570b3173d5af2874
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2012-10-27 18:37:58 +0400

    add AssociationLength to autoload

--- lib/validates.rb
+++ lib/validates.rb
@@ -1,8 +1,13 @@
-[:email, :existence, :inn, :money, :slug, :url].each do |name|
-  autoload :"#{name.capitalize}Validator", "#{name}_validator"
-end
-
 require 'active_model' # why i need do it?
+
+autoload :EmailValidator, "email_validator"
+autoload :ExistenceValidator, "existence_validator"
+autoload :InnValidator, "inn_validator"
+autoload :MoneyValidator, "money_validator"
+autoload :SlugValidator, "slug_validator"
+autoload :UrlValidator, "url_validator"
+autoload :AssociationLengthValidator, "association_length_validator"
+
 
 module Validates
 end
