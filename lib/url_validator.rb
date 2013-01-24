2413569742991ece77d186f438c3f39d13309c47
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-01-24 18:15:25 +0400

    Fixing trouble with spaces after url

--- lib/url_validator.rb
+++ lib/url_validator.rb
@@ -1,7 +1,17 @@
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ URI::regexp(%w(http https))
+    unless self.class.valid_url?(value)
       record.errors.add(attribute, :url, options.merge(:value => value))
+    end
+  end 
+  class << self
+    def valid_url?(value)
+      #value =~ /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/
+      uri = URI.parse value
+      uri.kind_of? URI::HTTP
+      rescue URI::InvalidURIError
+        false
     end
   end
 end
+
