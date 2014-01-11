4b632549df6e85570cb897e34e76429a44fb933e
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-01-11 04:14:40 +0400

    InnValidator

--- lib/inn_validator.rb
+++ lib/inn_validator.rb
@@ -1,28 +1,34 @@
 class InnValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, inn)
-    result = false
-    p10 = [2, 4, 10, 3, 5, 9, 4, 6, 8]
-    p11 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
-    p12 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
-    inn = inn.gsub(/\D/, "") unless inn.nil?
-    inn ||= []
+  def validate_each(record, attribute, value)
+    unless valid?(value)
+      record.errors.add(attribute, :inn, options.merge(value: value))
+    end
+  end
+
+  private
+
+  def valid?(inn)
+    inn = inn.to_s.split(//).map(&:to_i)
+
     if inn.length == 10
-      n10 = calc(p10, inn)
-      result = (n10 == inn[9].to_i)
+      n10 = calc(P10, inn)
+      return true if n10 == inn[9]
     end
 
     if inn.length == 12
-      n11 = calc(p11, inn)
-      n12 = calc(p12, inn)
-      result = (n11 == inn[10].to_i) && (n12 == inn[11].to_i)
+      n11 = calc(P11, inn)
+      n12 = calc(P12, inn)
+      return true if n11 == inn[10] && n12 == inn[11]
     end
 
-    unless result
-      record.errors.add(attribute, :inn, options.merge(:value => inn))
-    end
+    false
   end
 
   def calc(p, inn)
-    p.to_enum(:each_with_index).inject(0){|sum, elem| sum + elem[0].to_f * inn[elem[1]].to_f}%11%10
+    p.each_with_index.inject(0){ |s, p| s + p[0] * inn[p[1]] } % 11 % 10
   end
+
+  P10 = [2, 4, 10, 3, 5, 9, 4, 6, 8]
+  P11 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+  P12 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
 end
